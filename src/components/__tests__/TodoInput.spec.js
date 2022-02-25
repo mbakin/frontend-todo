9bfd30b4a4c9a6acc6f8f57aba7f69bb12ab57b6
mbakin <mehmetberkakinn@outlook.com>
2022-02-26 01:38:14 +0300

    test: unit test for TodoInput, tests red

--- src/components/__tests__/TodoInput.spec.js
+++ src/components/__tests__/TodoInput.spec.js
@@ -8,8 +8,14 @@ describe("TodoInput.vue", () => {
     
     const addButton = wrapper.find("#add-todo-button");
     expect(addButton.exists()).toBe(true);
+  
+    const buttonText = addButton.text();
+    expect(buttonText).toBe("Add Todo Item");
     
     const todoInput = wrapper.find("#add-todo-input");
     expect(todoInput.exists()).toBe(true);
+  
+    const inputInitialValue = todoInput.text();
+    expect(inputInitialValue).toBe("");
   });
 })
