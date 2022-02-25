1c5796c226cb869ee76310d998bba75fc76eafd5
mbakin <mehmetberkakinn@outlook.com>
2022-02-26 00:44:54 +0300

    test: should render correctly TodoList.vue component is red

--- src/views/__tests__/Layout.spec.js
+++ src/views/__tests__/Layout.spec.js
@@ -2,6 +2,7 @@ import {shallowMount} from "@vue/test-utils";
 import Layout from "@/views/Layout.vue";
 import TodoList from "@/components/TodoList";
 import TodoInput from "@/components/TodoInput";
+import flushPromises from "flush-promises";
 import API from "@/api";
 
 jest.mock('@/api')
@@ -21,5 +22,34 @@ describe("Layout.vue", () => {
     const wrapper = shallowMount(Layout);
     const list = wrapper.find(".todos");
     expect(list.exists()).toBe(true);
+  });
+  it('should TodoList component exists', async () => {
+    API.getTodos.mockResolvedValue([])
+    const wrapper = shallowMount(Layout);
+    expect(wrapper.exists()).toBeTruthy()
+  });
+  
+  it('should render TodoList item component correctly', async () => {
+    const mockResponse = [
+      {
+        id: 1,
+        title: 'mock test item1',
+      },
+      {
+        id: 2,
+        title: 'mock test item2',
+      }, {
+        id: 1,
+        title: 'mock test item3',
+      }
+    ]
+    
+    API.getTodos.mockResolvedValue(mockResponse)
+    const wrapper = shallowMount(Layout);
+    
+    await flushPromises();
+    const todoListComponent = wrapper.findComponent(TodoList);
+    expect(todoListComponent.exists()).toBeTruthy();
+    
   });
 });
