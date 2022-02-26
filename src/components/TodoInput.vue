c6c5472961658ebd6f3f260a69bce90cbe206c50
mbakin <mehmetberkakinn@outlook.com>
2022-02-26 15:20:11 +0300

    feat: TodoInput.vue methods unit tests green

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <input type="text" id="add-todo-input">
-    <button id="add-todo-button">Add Todo Item</button>
+    <input type="text" id="add-todo-input" v-model="todo">
+    <button id="add-todo-button" @click="addTodo">Add Todo Item</button>
   </div>
 </template>
 
 <script>
+
+
 export default {
-  name: "TodoInput"
+  name: "TodoInput",
+  data() {
+    return {
+      todo: "",
+      todos: ""
+    }
+  },
+  methods: {
+    async addTodo() {
+      if (this.todo === ""){
+        this.todos = "";
+        return
+      }
+    }
+  }
 }
 </script>
 
